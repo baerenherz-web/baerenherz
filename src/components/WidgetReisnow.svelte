07805b66a3f320db3895f1d0c7ac454acee52ce5
darayaq-01 <daniel@darayaq.de>
2023-09-12 17:51:20 +0200

    feat: spenden widget

---
+++ src/components/WidgetReisnow.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      const script1 = document.createElement('script');
+      script1.src = 'https://tamaro.raisenow.com/brenh-1353/latest/widget.js';
+      document.body.appendChild(script1);
+  
+      script1.onload = () => {
+        window.rnw.tamaro.runWidget('.rnw-widget-container');
+      };
+    });
+  </script>
+  
+  <div class="rnw-widget-container"></div>
+  
