b26dc0ab557a548e00e2530b0011c9b320c9e681
darayaq-01 <daniel@darayaq.de>
2023-09-13 04:24:52 +0200

    add: spinner to widget

--- src/components/WidgetReisnow.svelte
+++ src/components/WidgetReisnow.svelte
@@ -1,16 +1,40 @@
 <script>
-    import { onMount } from 'svelte';
-  
-    onMount(() => {
-      const script1 = document.createElement('script');
-      script1.src = 'https://tamaro.raisenow.com/brenh-1353/latest/widget.js';
-      document.body.appendChild(script1);
-  
-      script1.onload = () => {
-        window.rnw.tamaro.runWidget('.rnw-widget-container');
-      };
-    });
-  </script>
-  
+  import { onMount, afterUpdate } from 'svelte';
+
+  let isLoading = true; 
+
+  onMount(() => {
+    const script1 = document.createElement('script');
+    script1.src = 'https://tamaro.raisenow.com/brenh-1353/latest/widget.js';
+
+    script1.onload = () => {
+      window.rnw.tamaro.runWidget('.rnw-widget-container');
+      isLoading = false; 
+    };
+
+    document.body.appendChild(script1);
+  });
+
+  afterUpdate(() => {
+    setTimeout(() => {
+      if (isLoading) {
+        isLoading = false;
+      }
+    }, 5000); // 5000 milisegundos (5 segundos)
+  });
+</script>
+
+{#if isLoading}
+  <div class="flex justify-center p-8 transition-shadow">
+  <div
+  class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+  role="status">
+  <span
+    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span
+  >
+  </div>
+</div>
+{:else}
   <div class="rnw-widget-container"></div>
-  
+{/if}
