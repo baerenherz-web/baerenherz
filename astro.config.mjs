95ee7758299186f0c16fb2e71bd330b452836232
TimMazh <tim@mazhari.ch>
2024-01-06 16:55:10 +0100

    Added / updated content. Edited config for local

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/functions"; //for local server deploy change to @astrojs/netlify
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
@@ -9,6 +9,8 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), preact(), react(), svelte()],
+
+  //for local server deploy comment out experimental section
   experimental: {
     assets: true
   },
